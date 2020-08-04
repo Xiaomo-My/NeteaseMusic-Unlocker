b1aa76531c11055d0d85249d958d98cc660f317a
NULL204 <69097698+NULL204@users.noreply.github.com>
2020-08-04 16:29:41 +0800

    Add files via upload

--- NeteaseMusic Unlocker.bat
+++ NeteaseMusic Unlocker.bat
@@ -8,6 +8,16 @@ echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
 echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
 "%temp%\getadmin.vbs"
 exit /B
+@echo off
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
 :gotAdmin
 if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
 if /i "%PROCESSOR_IDENTIFIER:~0,3%"=="x86" (set "bit=x86") else (set "bit=x64")
@@ -22,19 +32,19 @@ DEL /F /Q "%TEMP%\disablequickedit.reg"
 start "" "cmd" /c "%~dpnx0"&exit
 :mainstart
 cd /d %~dp0files
-title NeteaseMusic Unlocker 修改 by NULL ^& 小宇
+title NeteaseMusic-Unlocker
 mode con lines=30 cols=120
 :mouse
 cls
 echo.
 echo.
-echo        ###     ## ##      ## ##      ## ###     ## ##        #######   ######  ##    ##  ######## ########  
-echo        ####    ## ###    ### ##      ## ####    ## ##       ##     ## ##    ## ##   ##   ##       ##     ## 
-echo        ## ##   ## ####  #### ##      ## ## ##   ## ##       ##     ## ##       ##  ##    ##       ##    ## 
-echo        ##  ##  ## ## #### ## ##      ## ##  ##  ## ##       ##     ## ##       #####     ######   ########  
-echo        ##   ## ## ##  ##  ## ##      ## ##   ## ## ##       ##     ## ##       ##  ##    ##       ##   ## 
-echo        ##    #### ##      ## ##      ## ##    #### ##       ##     ## ##    ## ##   ##   ##       ##    ## 
-echo        ##     ### ##      ##  ########  ##     ### ########  #######   ######  ##    ##  ######## ##     ## 
+echo        ###     ## ##      ## ##      ## ###     ## ##        #######   ######  ##    ##  ######## ########
+echo        ####    ## ###    ### ##      ## ####    ## ##       ##     ## ##    ## ##   ##   ##       ##     ##
+echo        ## ##   ## ####  #### ##      ## ## ##   ## ##       ##     ## ##       ##  ##    ##       ##    ##
+echo        ##  ##  ## ## #### ## ##      ## ##  ##  ## ##       ##     ## ##       #####     ######   ########
+echo        ##   ## ## ##  ##  ## ##      ## ##   ## ## ##       ##     ## ##       ##  ##    ##       ##   ##
+echo        ##    #### ##      ## ##      ## ##    #### ##       ##     ## ##    ## ##   ##   ##       ##    ##
+echo        ##     ### ##      ##  ########  ##     ### ########  #######   ######  ##    ##  ######## ##     ##
 echo.
 echo.
 echo                ===================================================================================
@@ -56,21 +66,26 @@ set /a Y=%xy%-1000*%X%
 if %y% gtr 18 if %y% lss 22 if %x% gtr 44 if %x% lss 70 goto start
 goto mouse
 :start 
-if not exist C:\"Program Files (x86)"\Netease\CloudMusic\ ( goto nosoftware )
-set software=C:\"Program Files (x86)"\Netease\CloudMusic\
-set lnkaddress="C:\Program Files (x86)\Netease\CloudMusic\
-goto node
+cls
+for /f "tokens=2* " %%i in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Netease\cloudmusic" /v "install_dir"') do set software=%%j
+if "%software%"=="" ( goto nosoftware ) else ( goto node )
 :nosoftware
 cls
-set /p software=请将网易云音乐安装目录拖入此处（cloudmusic.exe所在目录，如C:\Program Files (x86)\Netease\CloudMusic）:
-echo 目录为：%software%，请谨慎确认，否则会造成失败
-echo 如有选错，请重启此脚本，重新选择
+echo.
+echo 无法自动获取网易云音乐安装目录，请手动输入!
+echo 格式如C:\Program Files ^(x86^)\Netease\CloudMusic，不要引号，最后不要加“\”
+set /p software=目录为：
+echo 您输入的目录为：%software%
+echo 请检查是否正确！
 pause
 :node
 node
-if %ERRORLEVEL% == 0 ( goto next ) else ( goto id )
+if %ERRORLEVEL% == 0 ( goto next ) else ( goto nonode )
 :next
-NSudoLG.exe -Wait -U:S -P:E 7za.exe x UNMfiles1.7z -o%software%
+cls
+echo.
+echo 正在解压文件
+NSudoLG.exe -Wait -ShowWindowMode:Hide -U:S -P:E 7za.exe x UNMfiles1.7z -aoa -o"%software%"
 if %ERRORLEVEL% == 0 ( goto 1 ) else ( 
     cls
     echo.
@@ -78,7 +93,7 @@ if %ERRORLEVEL% == 0 ( goto 1 ) else (
     pause
     goto next
  )
-:id
+:nonode
 cls
 echo.
 echo 未发现Node.js，是否加入已经集成的Node.js（不会影响其他程序）？（yes/no）
@@ -94,7 +109,7 @@ if "%id%"=="No" goto no
 if "%id%"=="NO" goto no
 if "%id%"=="n" goto no
 if "%id%"=="N" goto no
-goto id
+goto nonode
 :no
 cls
 echo.
@@ -103,29 +118,35 @@ echo 按任意键退出...
 pause >nul
 exit
 :yes
-NSudoLG.exe -Wait -U:S -P:E 7za.exe x UNMfiles2.7z -o%software%
+cls
+echo.
+echo 正在解压文件
+NSudoLG.exe -Wait -ShowWindowMode:Hide -U:S -P:E 7za.exe x UNMfiles2.7z -aoa -o"%software%"
 if %ERRORLEVEL% == 0 ( goto 1 ) else ( 
     cls
     echo.
     echo 解压失败，请检查是否有权限，并重试
-    pause
-    goto yes
+    echo 按任意键退出...
+    pause >nul
+    exit
  )
 :1
-cls
-echo.
+echo 正在创建桌面快捷方式
+for /f "tokens=2* " %%i in ('reg query "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "Desktop"') do set Dsektop=%%j
 del C:\Users\Public\Desktop\网易云音乐.lnk
-echo oShellLink.TargetPath = %lnkaddress%Start.exe" >> Shortcut.vbs
+echo set oShellLink = WshShell.CreateShortcut("%Dsektop%\网易云音乐.lnk") >> Shortcut.vbs
+echo oShellLink.TargetPath = "%software%\Start.exe" >> Shortcut.vbs
 echo oShellLink.WindowStyle = 1 >> Shortcut.vbs
-echo oShellLink.IconLocation = %lnkaddress%cloudmusic.exe, 0" >> Shortcut.vbs
+echo oShellLink.IconLocation = "%software%\cloudmusic.exe, 0" >> Shortcut.vbs
 echo oShellLink.Description = "网易云音乐" >> Shortcut.vbs
-echo oShellLink.WorkingDirectory = %lnkaddress%" >> Shortcut.vbs
+echo oShellLink.WorkingDirectory = "%software%" >> Shortcut.vbs
 echo oShellLink.Save >> Shortcut.vbs
+NSudoLG.exe -U:S -P:E Shortcut.vbs
 start Shortcut.vbs
-start killcloudmusic.bat
-%software%cloudmusic.exe
+echo 正在更改设置代理服务器设置
+start /B killcloudmusic.bat
+"%software%\cloudmusic.exe"
 MOVE /Y Config C:\Users\"%username%"\AppData\Local\Netease\CloudMusic
-start %software%Start.exe
 cls
 echo.
 echo 脚本运行完成！
